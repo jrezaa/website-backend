424c72585968b70b951ba64b3251f96ddb109ffe
Jeff <jeffrezazada@gmail.com>
2024-09-08 20:39:10 -0400

    added multiple endpoints

--- website-backend-flask/app.py
+++ website-backend-flask/app.py
@@ -1,83 +1,44 @@
-from flask import Flask, request
-from flask_smorest import abort
-from db import items, stores
-import uuid
+import os
 
-app = Flask(__name__)
-
-
-@app.get("/store")
-def get_stores():
-    return {"stores": list(stores.values())}
-
-
-@app.post("/store")
-def create_store():
-    store_data = request.get_json()
-    store_id = uuid.uuid4().hex
-    if not "name" in store_data:
-        abort(400, message="Request needs 'name' attribute.")
-    if foundDuplicateStore(store_data, stores):
-        abort(400, message=f"Store '{store_data["name"]}', already exists")
-    store = {**store_data, "id": store_id}
-    stores[store_id] = store
-    return store, 201
-
-def foundDuplicateStore(new_store, stores):
-    for store in stores:
-        if new_store["name"] == store["name"]: return True
-    return False
-
-def foundDuplicate(new_item, items):
-
-    for item in items:
-        if new_item["name"] == item and new_item["store_id"] == item["store_id"]:
-            return True
-
-    return False
+from flask import Flask
+from flask_smorest import Api
+from db import db
+import models
+from resources.item import blp as ItemBlueprint
+from resources.store import blp as StoreBlueprint
+from resources.tag import blp as TagBlueprint
+from resources.user import blp as UserBlueprint
+from flask_jwt_extended import JWTManager
 
 
-@app.post("/item")
-def create_item():
-    item_data = request.get_json()
-    if (
-        "price" not in item_data
-        or "store_id" not in item_data
-        or "name" not in item_data
-    ):
-        abort(
-            400,
-            message="Bad Request. Ensure 'price', 'store_id' and 'name' are inclued in JSON payload.",
-        )
+def create_app(db_url=None):
+    app = Flask(__name__)
 
-    if foundDuplicate(item=item_data, items=items):
-        abort(400, message=f"Item already exists")
-    if item_data["store_id"] not in stores:
-        abort(404, message="Store not found")
+    app.config["PROPAGATE_EXCEPTIONS"] = True
+    app.config["API_TITLE"] = "Stores REST API"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    app.config["OPENAPI_URL_PREFIX"] = "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config["OPENAPI_SWAGGER_UI_URL"] = (
+        "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    )
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv(
+        "DATABASE_URL", "sqlite:///data.db"
+    )
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
 
-    item_id = uuid.uuid4().hex
-    item = {**item_data, "id": item_id}
-    items[item_id] = item
+    api = Api(app)
 
-    return item, 201
+    app.config["JWT_SECRET_KEY"] = "149971902632461760422733320754140327645"
+    jwt = JWTManager(app)
 
+    with app.app_context():
+        db.create_all()
 
-@app.get("/item")
-def get_all_items():
-    return {"items": list(items.values())}
-
-
-@app.get("/store/<string:store_id>")
-def get_store(store_id: str):
-    try:
-        return stores[store_id], 200
-    except KeyError:
-        abort(404, message="Store not found.")
-
-
-@app.get("/item/<string:item_id>")
-def get_item(item_id):
-    try:
-        return items[item_id], 200
-    except KeyError:
-        abort(404, message="Item not found.")
+    api.register_blueprint(ItemBlueprint)
+    api.register_blueprint(StoreBlueprint)
+    api.register_blueprint(TagBlueprint)
+    api.register_blueprint(UserBlueprint)
+    return app
